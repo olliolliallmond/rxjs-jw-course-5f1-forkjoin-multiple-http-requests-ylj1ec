4216c917733534745ac78d36419961a6bd0db60b
olliolliallmond <aallmondcar@gmail.com>
2021-08-08 13:42:51 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rxjs-jw-course-5f1-forkjoin-multiple-http-requests-ylj1ec
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/rxjs-jw-course-5f1-forkjoin-multiple-http-requests-ylj1ec)
---
+++ index.ts
@@ -0,0 +1,31 @@
+import { forkJoin } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+
+const randomName$ = ajax('https://random-data-api.com/api/name/random_name');
+
+const randomNation$ = ajax(
+  'https://random-data-api.com/api/nation/random_nation'
+);
+
+const randomFood$ = ajax('https://random-data-api.com/api/food/random_food');
+
+// randomName$.subscribe(ajaxResponse =>
+//   console.log(ajaxResponse.response.first_name)
+// );
+// randomNation$.subscribe(ajaxResponse =>
+//   console.log(ajaxResponse.response.capital)
+// );
+// randomFood$.subscribe(ajaxResponse => console.log(ajaxResponse.response.dish));
+
+// forkJoin([randomName$, randomNation$, randomFood$]).subscribe(ajaxResponses =>
+//   console.log(ajaxResponses)
+// );
+
+// array destructuring
+forkJoin([randomName$, randomNation$, randomFood$]).subscribe(
+  ([nameAjax, nationAjax, foodAjax]) =>
+    console.log(`
+  ${nameAjax.response.first_name} is from ${
+      nationAjax.response.capital
+    } and likes to eat ${foodAjax.response.dish}`)
+);
